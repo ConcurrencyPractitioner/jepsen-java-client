cd3f910d82067fff882c6146379b08322dbf729c
ConcurrencyPractitioner <yohan.richard.yu@gmail.com>
2019-12-26 11:43:49 -0800

    Moving file

---
+++ src/user/jepsen/JepsenExecutable.java
@@ -0,0 +1,10 @@
+package user.jepsen;
+
+import clojure.lang.ArraySeq;
+//import jepsen.clj.main;
+
+public class JepsenExecutable {
+    public static void main(String[] args) {
+         System.out.println("Running java");
+    }
+}
