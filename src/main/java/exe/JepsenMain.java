668ae0a098a978b3ab0e85efada3309d560351ad
ConcurrencyPractitioner <yohan.richard.yu@gmail.com>
2019-12-27 21:24:04 -0800

    Making packages easier

--- src/main/java/exe/JepsenMain.java
+++ src/main/java/exe/JepsenMain.java
@@ -1,9 +1,11 @@
+package exe;
+
 import java.io.*;
 import clojure.lang.ArraySeq;
 import user.jepsen.JepsenExecutable;
 
 public class JepsenMain {
     public static void main(String[] args) {
-    	
+    	System.out.println("Hello, this is the main class being executed!");
     }
 }
